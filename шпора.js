6a12d46dad8c6941f190ff4983baee31b842201b
Александр <westerovs@mail.ru>
2021-02-02 22:11:44 +0300

    install PUG

--- шпора.js
+++ шпора.js
@@ -1,15 +1,14 @@
-
 const http = require('http') // подключить модуль 'http'
 const fs   = require('fs')   // подключить модуль fs
 
-
-http.creatrServer() // запустить модуль
+http.createServer() // запустить модуль
 
 http.createServer((request, response) => {
     response.end('HELLO !!!')
 }).listen(3000)
 
-
+// запуск дебагера
+// DEBUG=express:router node app.js
 
 
 
