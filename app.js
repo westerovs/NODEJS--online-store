53c203fc766b2c78e6563b8d29f9d8b77fd5362c
Александр <westerovs@mail.ru>
2021-02-04 23:25:46 +0300

    добавил пÐheader и main pug

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 // node app.js - run
 const express = require('express')
 const app = express()
+// const pug = require('pug')
 
 // public - имя папки, где хранится статика
 app.use(express.static('public'))
+app.set('view engine', 'pug') // подключить pug
+
 
 // слушать порт по умолчанию
-app.listen(3000, () => {
-    console.log('*** RUN SERVER ***')
-})
-
-app.get('/xxx', (req, res) => {
-    console.log('load /')
-    res.render('index.html')
-})
+app.listen(3000, () => console.log('*** RUN SERVER ***'))
+app.get('/pug', (req, res) => res.render('main.pug'))
 
